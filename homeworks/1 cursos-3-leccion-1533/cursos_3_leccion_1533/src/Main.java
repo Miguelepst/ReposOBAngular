a693000cfa6153d21d4840e51e504d85b99d9bf4
Miguel <miguelepst@hotmail.com>
2022-11-26 16:09:20 -0500

    Ejercicios tema 3
    Primera parte:
    
    Crear una función con tres parámetros que sean números que se suman entre sí.
    
    Llamar a la función en el main y darle valores.
    
    Segunda parte:
    
    Crear una clase coche.
    
    Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
    
    Una función que incremente el número de puertas que tiene el coche.
    
    Crear un objeto miCoche en el main y añadirle una puerta.
    
    Mostrar el número de puertas que tiene el objeto.
    
    Funcional(Ok(OK)/Error(0))
    Error1!=""

--- homeworks/1 cursos-3-leccion-1533/cursos_3_leccion_1533/src/Main.java
+++ homeworks/1 cursos-3-leccion-1533/cursos_3_leccion_1533/src/Main.java
@@ -1,11 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Primera parte
         System.out.println(funSuma(2, 2, 2));
+
+        // Segunda parte:
+        Coche objMicoche = new Coche();
+        System.out.println("Numero de puestas: " + objMicoche.numPuertas);
+        objMicoche.incrementarNumPuertas();
+        objMicoche.incrementarNumPuertas();
+        objMicoche.incrementarNumPuertas();
+        System.out.println("Numero de puestas: " + objMicoche.numPuertas);
+
     }
 
     public static int funSuma(int a, int b, int c) {
         return a + b + c;
 
+    }
+}
+
+class Coche {
+    int numPuertas = 2;
+
+    /* Constructor */
+    public Coche() {
+    }
+
+    public void incrementarNumPuertas() {
+        numPuertas++;
     }
 }
