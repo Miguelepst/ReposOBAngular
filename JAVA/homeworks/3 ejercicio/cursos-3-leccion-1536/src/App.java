b2f81a1d6d415cd669a61e3cd110c8330d8151b6
Miguel <miguelepst@hotmail.com>
2022-11-29 13:49:57 -0500

    Ejercicio #2  Resuelto https://campus.open-bootcamp.com/cursos/3/leccion/1536

---
+++ JAVA/homeworks/3 ejercicio/cursos-3-leccion-1536/src/App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ejercicios tema 8");
+        Persona persona = new Persona();
+        persona.setNombre("Miguel");
+        persona.setdad(43);
+        persona.setTelefono("+57 312 487 2877");
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Nombre: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getTelefono());
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefeno;
+
+    public void setdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefeno = telefono;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getTelefono() {
+        return this.telefeno;
+    }
+
+}
