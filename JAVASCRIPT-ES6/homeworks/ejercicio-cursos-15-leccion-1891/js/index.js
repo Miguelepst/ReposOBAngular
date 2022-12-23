fb3e525d1994edd5cea0e86a61364bf2dc046329
Miguel <miguelepst@hotmail.com>
2022-12-23 11:05:23 -0500

    commit

---
+++ JAVASCRIPT-ES6/homeworks/ejercicio-cursos-15-leccion-1891/js/index.js
@@ -0,0 +1,2 @@
+
+alert("Clic en el boton");
---
+++ JAVASCRIPT-ES6/primer-proyecto/Untitled-1.js
@@ -0,0 +1,21 @@
+/* Tipos primitivos */
+0
+1
+
+ "string";
+'cadena';
+`hola`;
+
+true;
+false;
+
+null;
+undefined;
+console.log("tes2");
+
+console.log("tes3");
+
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+} */
